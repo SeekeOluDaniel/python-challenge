fcebd277e72faf432939b3a99985a2e0a1f0d66b
codedshayx <seekeoludaniel@gmail.com>
2023-09-11 13:36:01 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,10 +5,8 @@ import csv
 election_path = "Resources\election_data.csv"
 
 # Create Sets as needed
-Candidate_Names = set()
-
-# Create Lists as needed
-# vote_count = []
+candidate_options = []
+candidate_votes = {}
 
 # Open file and read
 with open(election_path, "r") as csvfile:
@@ -17,21 +15,33 @@ with open(election_path, "r") as csvfile:
     csvheader = next(csvreader)
 
 # Define variables to be iterated over
-    rowcount = 0
-         
+    rowcount = 0 
 
     for row in csvreader:
         rowcount = rowcount + 1
         candidate = row[2]
+
+        if candidate not in candidate_options:
+            candidate_options.append(candidate)
+            candidate_votes[candidate] = 0
+        candidate_votes[candidate] = candidate_votes[candidate] + 1
+
+    print(f"Total Votes: ", rowcount)
+
+    total_votes = float(369711)
+    winning_count = 0
+    
+    for candidate in candidate_votes:
+
+        votes = candidate_votes.get(candidate)
+        percentage_won = float(votes) / float(total_votes) * 100       
         
-        if candidate != None and candidate != " ":
-            if candidate not in Candidate_Names:
-                Candidate_Names.add(candidate)
+        vote_results = f"{candidate}: {round(percentage_won, 3)}% ({votes})\n"
+        print(vote_results)
 
-        
-print(f"Total Votes: ", rowcount)
-print(Candidate_Names)
-
-
-
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+    
+print(f"The winner is: " + winning_candidate)
 
